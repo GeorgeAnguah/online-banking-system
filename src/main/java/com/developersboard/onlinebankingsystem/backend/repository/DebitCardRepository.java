2f961d9ea12b57bbe6208209796489842824f360
GeorgeAnguah <Georgeanguah@yahoo.com>
2018-04-24 21:25:09 -0400

    added fix-debitCard repository

--- src/main/java/com/developersboard/onlinebankingsystem/backend/repository/DebitCardRepository.java
+++ src/main/java/com/developersboard/onlinebankingsystem/backend/repository/DebitCardRepository.java
@@ -1,6 +1,7 @@
 package com.developersboard.onlinebankingsystem.backend.repository;
 
+import com.developersboard.onlinebankingsystem.backend.domain.DebitCard;
 import org.springframework.data.repository.CrudRepository;
 
-public interface DebitCardRepository extends CrudRepository {
+public interface DebitCardRepository extends CrudRepository<DebitCard, Long> {
 }
