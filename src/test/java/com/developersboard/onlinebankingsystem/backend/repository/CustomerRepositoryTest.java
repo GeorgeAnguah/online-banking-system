95e960a9418ff0fbfc1d5ed0808dc0351700cc5d
GeorgeAnguah <Georgeanguah@yahoo.com>
2018-04-24 22:52:51 -0400

    adding customerRepo integrationTest

---
+++ src/test/java/com/developersboard/onlinebankingsystem/backend/repository/CustomerRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.developersboard.onlinebankingsystem.backend.repository;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerRepositoryTest {
+
+
+
+
+}
