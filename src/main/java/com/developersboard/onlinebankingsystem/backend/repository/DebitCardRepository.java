911ee9f04f6533613a40e44851b293825ff8d23b
GeorgeAnguah <Georgeanguah@yahoo.com>
2018-04-24 20:42:41 -0400

    added customer and DebitCard repository

---
+++ src/main/java/com/developersboard/onlinebankingsystem/backend/repository/DebitCardRepository.java
@@ -0,0 +1,6 @@
+package com.developersboard.onlinebankingsystem.backend.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface DebitCardRepository extends CrudRepository {
+}
