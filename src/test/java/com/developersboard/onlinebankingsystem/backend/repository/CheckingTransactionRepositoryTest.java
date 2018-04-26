de0908ae2c08500f6f37a0a5f4ffb1e82ee010a2
GeorgeAnguah <Georgeanguah@yahoo.com>
2018-04-26 16:12:26 -0400

    added customerRepository-IntegrationTest

---
+++ src/test/java/com/developersboard/onlinebankingsystem/backend/repository/CheckingTransactionRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.developersboard.onlinebankingsystem.backend.repository;
+
+import com.developersboard.onlinebankingsystem.OnlineBankingSystemApplicationTests;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CheckingTransactionRepositoryTest extends OnlineBankingSystemApplicationTests {
+
+    @Test
+    void test() {
+
+    }
+}
--- src/test/java/com/developersboard/onlinebankingsystem/backend/repository/CustomerRepositoryIntegrationTest.java
+++ src/test/java/com/developersboard/onlinebankingsystem/backend/repository/CustomerRepositoryIntegrationTest.java
@@ -1,13 +1,13 @@
 package com.developersboard.onlinebankingsystem.backend.repository;
 
 import com.developersboard.onlinebankingsystem.OnlineBankingSystemApplication;
+import com.developersboard.onlinebankingsystem.OnlineBankingSystemApplicationTests;
 import com.developersboard.onlinebankingsystem.backend.domain.Customer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -15,14 +15,15 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest (
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-        classes =  OnlineBankingSystemApplication.class
-)
+//@RunWith(SpringRunner.class)
+//@SpringBootTest (
+//        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+//        classes =  OnlineBankingSystemApplication.class
+//)
+//
+//@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 
-@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
-public class CustomerRepositoryIntegrationTest {
+public class CustomerRepositoryIntegrationTest extends OnlineBankingSystemApplicationTests {
 
     @Autowired
     private CustomerRepository customerRepository;
@@ -31,11 +32,12 @@ public class CustomerRepositoryIntegrationTest {
     public void testFindByEmail() {
 
         // setup
+        // Create and save customer record
         Customer aNewCustomer = new Customer();
         aNewCustomer.setEmail("georgeanguah@yahoo.com");
         customerRepository.save(aNewCustomer);
 
-        // Create and save customer record
+        // retrieve customer from db
         Customer foundContact = customerRepository.findByEmail("georgeanguah@yahoo.com");
 
         // Assertion
